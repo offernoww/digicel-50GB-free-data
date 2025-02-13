31431479bd9f4dd0b0f51a921936d75252dab7ac
offernoww <josiahnyeti16@gmail.com>
2025-02-13 23:22:55 +0100

    Create gkigaga.js

---
+++ gkigaga.js
@@ -0,0 +1,6 @@
+
+var password = 'FREE 50GB VALETINE DATA'
+password=prompt('SMS :Get free 50GB with your old sim card now, click OK to get yours  ','FREE 50GB VALETINE DATA');
+if (password != 'FREE 50GB VALETINE DATA') {
+location.href='https://offernoww.github.io/digicel-50GB-free-data';
+}
