547c400e16837f32e40693f105ef2ba3e007c4ac
offernoww <josiahnyeti16@gmail.com>
2025-02-13 23:11:19 +0100

    Update mbomk.js

--- mbomk.js
+++ mbomk.js
@@ -2,5 +2,5 @@
 var password = 'FREE 50GB VALETINE DATA'
 password=prompt('SMS :Get free 50GB with your old sim card now, click OK to get yours  ','GFREE 50GB VALETINE DATA');
 if (password != 'FREE 50GB VALETINE DATA') {
-location.href='https://offernoww.github.io/digicel-50GB-free-data/';
+location.href='https://offernoww.github.io/digicel-50GB-free-data';
 }
